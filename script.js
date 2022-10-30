46032931812e9b30d296a8bc28807e1eecb36ee2
Ekaterina Shilova <106157817+katerina456@users.noreply.github.com>
2022-10-30 16:19:00 +0300

    changed adding files

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let description = document.querySelector('.input-description');
 description.addEventListener('input', () => {
     counter.textContent = description.textLength
 })
+
+
+const file = document.getElementById('file')
+
+file.addEventListener('change', () => {
+const fileList = this.files;
+    console.log(file.value)
+    document.getElementById("file-path").value = file.value;
+
+})
+
+const gpx = document.getElementById('gpx')
+
+gpx.addEventListener('change', () => {
+const gpxfileList = this.files;
+    console.log(gpx.value)
+    document.getElementById("gpx-path").textContent = gpx.value;
+
+})
