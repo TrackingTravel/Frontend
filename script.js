fb15bd311837a7d14cae8a510a51722a9d26eacf
Ekaterina Shilova <106157817+katerina456@users.noreply.github.com>
2022-10-31 21:42:03 +0300

    hover for buttons

--- script.js
+++ script.js
@@ -6,20 +6,60 @@ description.addEventListener('input', () => {
 })
 
 
-const file = document.getElementById('file')
+const btn = document.querySelector('.button-foto')
+
+btn.addEventListener('mouseover', () => {
+    btn.innerHTML = '<img src="plus_hover.svg" alt="plus">'
+})
+
+
+btn.addEventListener('mouseout', () => {
+    btn.innerHTML = '<img src="plus.svg" alt="plus">'
+})
+
+
+btn.addEventListener('click', () => {
+    let check = document.querySelectorAll('.file-path')
+    if (check[check.length-1].value == '') return
+
+    let file = document.querySelector('.file')
+
+    let input = document.createElement('input')
+    input.classList.add('input', 'file-path')
+    input.setAttribute('type', 'text')
+    input.setAttribute('placeholder', 'Ссылка')
+    /* input.title = 'tut' */
+
+    file.append(input)
+})
+
+
+/* const file = document.getElementById('file')
 
 file.addEventListener('change', () => {
 const fileList = this.files;
     console.log(file.value)
     document.getElementById("file-path").value = file.value;
 
-})
+}) */
 
 const gpx = document.getElementById('gpx')
 
 gpx.addEventListener('change', () => {
-const gpxfileList = this.files;
+    //const gpxfileList = this.files;
     console.log(gpx.value)
     document.getElementById("gpx-path").textContent = gpx.value;
-
 })
+
+
+const download = document.querySelector('.button-gpx')
+
+download.addEventListener('mouseover', () => {
+    download.innerHTML = '<img src="clip_hover.svg" alt="clip" class="clip"> Загрузить файл'
+})
+
+
+download.addEventListener('mouseout', () => {
+    download.innerHTML = '<img src="clip.svg" alt="clip" class="clip"> Загрузить файл'
+})
+
