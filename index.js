d64a614311cb9c2455492e63f4b7cbf7ad7ef42c
Ekaterina Shilova <106157817+katerina456@users.noreply.github.com>
2022-12-22 17:18:52 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,17 +6,46 @@ btns.forEach(btn => {
   })
 }) */
 
+document.addEventListener("DOMContentLoaded", getAllRoads);
 
-
-let btnGetAll = document.querySelector('.button-getAll')
-
-btnGetAll.addEventListener('click', () => {
-    
-    fetch('https://api.trackingtravel.me/test-routes', {
-      method: 'GET',
+function getAllRoads() {
+  fetch('https://api.trackingtravel.me/test-routes', {
+    method: 'GET',
       /* headers: { 'Content-Type': 'application/json' },
       credentials: "omit" */
+  })
+
+  .then(response => response.json())
+
+  .then(data => {
+    console.log(data)
+    let result = document.querySelector('.result')
+    result.innerHTML = ''
+    let main = document.querySelector('.main')
+    data.forEach(item => {
+      //setResult(item, result)
+      createSection(main, item)
     })
+
+  })
+
+  .catch(() => {
+    let result = document.querySelector('.result')
+    result.innerHTML = ''
+    result.innerHTML = '<p>Ошибка</p>'
+  })
+}
+
+
+/* let btnGetAll = document.querySelector('.button-getAll')
+
+btnGetAll.addEventListener('click', () => {
+  
+    
+  fetch('https://api.trackingtravel.me/test-routes', {
+    method: 'GET',
+      
+  })
 
   .then(response => response.json())
 
@@ -37,7 +66,7 @@ btnGetAll.addEventListener('click', () => {
     result.innerHTML = '<p>Ошибка</p>'
   })
 
-})
+}) */
 
   
 
@@ -166,4 +195,127 @@ function setResult(item, result) {
 
   result.append(box)
   result.append(document.createElement('br'))
+}
+
+
+
+function createSection(main, item) {
+  let section = document.createElement('section');
+  section.classList.add('container', 'section');
+ // debugger
+  section.append(createManagment(item));
+  
+  section.append(createTable(item));
+
+  let titleArray = ['Description', 'Photo'];
+  let valueArray = [item.description, item.photo];
+  section.append(addTablePart(titleArray, valueArray));
+
+  main.append(section);
+  //debugger
+}
+
+
+function createManagment(item) {
+  let management = document.createElement('div');
+  management.classList.add('management');
+
+  management.append(createManagmentItem(item));
+  management.append(addButtons())
+  //console.log('hi')
+  return management;
+}
+
+
+function createManagmentItem(item) {
+  let managementItem = document.createElement('div');
+  managementItem.classList.add('managmant-item');
+
+  let paragraph = document.createElement('p');
+  paragraph.classList.add('item-id');
+  paragraph.innerHTML = `ID <span id="id">${item.id}</span>`;
+
+  managementItem.append(paragraph);
+  return managementItem;
+}
+
+
+function addButtons() {
+  let div = document.createElement('div');
+  
+  let edit = addButton(['buttons', 'buttons-edit'], "./icon/edit.svg", "edit");
+  div.append(edit);
+
+  let del = addButton(['buttons', 'buttons-del'], "./icon/delete.svg", "delete");
+  div.append(del)
+  return div;
+}
+
+
+function addButton(classes, url, text) {
+  let button = document.createElement('button');
+  classes.forEach(clas => {
+    button.classList.add(clas);
+  })
+  
+  let img = document.createElement('img');
+  img.src = url;
+  img.alt = text;
+
+  button.append(img);
+  return button;
+}
+
+
+function createTable(item) { 
+  let table = document.createElement('div');
+  table.classList.add('table');
+  
+  let titleArray = ['Title', 'Country', 'Link to Map'];
+  let valueArray = [item.title, item.country.nameOfCountry, item.linkToMap];
+  table.append(addTablePart(titleArray, valueArray));
+
+  titleArray = ['Distance', 'Duration', 'Height Peak'];
+  valueArray = [item.distanceRoute, item.durationRoute, item.heightPeak];
+  table.append(addTablePart(titleArray, valueArray));
+  return table;
+}
+
+
+function addTablePart(titleArray, valueArray) {
+  let tablePart = document.createElement('div');
+  tablePart.classList.add('table-part');
+
+  for (let i = 0; i < titleArray.length; i++) {
+    tablePart.append(addTableItem(titleArray[i], valueArray[i]));
+  }
+
+  return tablePart
+}
+
+
+function addTableItem(title, text) {
+  let div = document.createElement('div');
+  div.classList.add('table-item');
+
+  let itemName = document.createElement('div');
+  itemName.classList.add('table-item-name');
+  itemName.textContent = title;
+  div.append(itemName);
+
+  let itemValue = document.createElement('div');
+  itemValue.classList.add('table-item-value');
+  if (title !== 'Link to Map') {
+    itemValue.textContent = text;
+  } else {
+    let link = document.createElement('a');
+    link.href = text
+    link.target = '_blank'
+    link.textContent = 'Link'
+    itemValue.append(link)
+  }
+  
+  div.append(itemValue);
+
+  return div;
 }
